0e9985124508e89e15bec7511e6dd3dc12cad030
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-08-29 15:30:30 -0300

    wrote function that hits api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-alert(2);
+const getWeatherInfo = async (city) => {
+  try {
+    console.log('Waiting for fetch to complete');
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=c59d34581182187599601a68c72dcacd`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Error: ' + error);
+  }
+};
+
+const asd = document.querySelector('.asd');
+asd.addEventListener('click', () => {
+  getWeatherInfo('tokyo').then((response) => {
+    console.log(
+      `Weather in ${response.name}, ${response.sys.country}: ${Math.round(
+        response.main.temp - 273
+      )} °C`
+    );
+  });
+});
