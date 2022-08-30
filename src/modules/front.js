58d1a95e2660ae6347f40dbaf60d4399c0571140
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-08-30 12:41:54 -0300

    functionality done

--- src/modules/front.js
+++ src/modules/front.js
@@ -5,6 +5,7 @@ const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
 
 const drawInfo = (data) => {
+  //receives a promise
   data.then((information) => {
     name.textContent = information.name;
     temperature.textContent = `Temperature: ${information.temperature} °C`;
